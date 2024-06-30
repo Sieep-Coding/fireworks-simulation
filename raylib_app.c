e487a12b880b2cc3f960f51ffbeb10ae5dba7c5a
Sieep-Coding <nstamb6@gmail.com>
2024-06-30 19:11:20 -0400

    logic to create firework

--- raylib_app.c
+++ raylib_app.c
@@ -2,10 +2,26 @@
 #include <stdlib.h>
 #include "firework.h"
 
-void initFirwork(Firework *firework, int screen)
+void initFirwork(Firework *firework, int screenWidth, int screenHeight)
+{
+    firework->position = (Vector2){GetRandomValue(screenWidth / 4, 3 * screenWidth / 4), screenHeight};
+    firework->speed = (Vector2){(float)GetRandomValue(-20, 20) / 10.0f, -(float)GetRandomValue(20, 50) / 10.0f};
+    firework->color = (Color){GetRandomValue(50, 255), GetRandomValue(50, 255), GetRandomValue(50, 255), 255};
+    firework->explodeTime = GetRandomValue(30, 60);
+    firework->exploded = false;
 
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        firework->particles[i].position = firework->position;
+        firework->particles[i].speed = (Vector2){(float)GetRandomValue(-20, 20) / 10.0f, (float)GetRandomValue(-20, 20) / 10.0f};
+        firework->particles[i].color = firework->color;
+        firework->particles[i].lifespan = FIREWORK_LIFESPAN;
+        firework->particles[i].active = false;
+    }
+}
 
-int main(void) {
+int main(void)
+{
     const int screenWidth = 900;
     const int screenHeight = 750;
 
